95cc639c5b7313ded952acb71afeeecfd83c46b2
sabin18 <sabin6341@gmail.com>
2020-07-07 12:30:42 +0200

    add employee

--- src/helpers/checkId.js
+++ src/helpers/checkId.js
@@ -1,9 +1,7 @@
 import idValidator from '../validation/idValidator';
 import responseUtil from '../Utils/responseUtil'
-// import strings from '../utils/stringsUtil';
 
 const { ErrorResponse } = responseUtil;
-// const { validateId } = InPutValidation;
 const checkId = (req, res, next) => {
   const {businessID} = req.params;
   const { error } = idValidator({ businessID })
